e7b749baa399eea02a35ca31b9d265367dd83f04
Alan Tavares <alan1tavares@gmail.com>
2021-07-15 21:22:07 -0300

    Adicionado a classe Modelo do Médico

---
+++ models/Medico.cs
@@ -0,0 +1,10 @@
+namespace ConsoleEFCore.models
+{
+  public class Medico
+  {
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Epecialidade { get; set; }
+    public int Idade { get; set; }
+  }
+}
