8b70b3a496f709235debca31a2bfc41e02d1bdfc
Alan Tavares <alan1tavares@gmail.com>
2021-07-15 22:15:51 -0300

    :sparkles: Adicionado o cadastro do médico

---
+++ Migrations/20210716010632_1.4.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace ConsoleEFCore.Migrations
+{
+    public partial class _14 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Enfermarias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Descricao = table.Column<string>(type: "text", nullable: true),
+                    NumeroDeLeitos = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Enfermarias", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Medicos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nome = table.Column<string>(type: "text", nullable: true),
+                    Especialidade = table.Column<string>(type: "text", nullable: true),
+                    Idade = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Medicos", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Medicos",
+                columns: new[] { "Id", "Especialidade", "Idade", "Nome" },
+                values: new object[] { 1, "Clínico geral", 39, "Roberto Souza" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Enfermarias");
+
+            migrationBuilder.DropTable(
+                name: "Medicos");
+        }
+    }
+}
